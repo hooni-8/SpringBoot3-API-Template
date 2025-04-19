5ef81f8ef0720b690603a8386a88d0c55489d30f
LeeSH <dltkdgns0726@naver.com>
2025-04-19 22:41:34 +0900

    - WebSecurityConfig authenticationManager 추가

--- src/main/java/org/template/template/config/WebSecurityConfig.java
+++ src/main/java/org/template/template/config/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package org.template.template.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,6 +33,12 @@ public class WebSecurityConfig {
         });
 
         http.addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
     }
 }
